49014a2de5a091dbc40829a0d254a3782ecc7410
Morgan Allen <morgan.allen@outlook.com>
2021-02-02 07:31:18 -0600

    Modules

--- Code/model/configuration.py
+++ Code/model/configuration.py
@@ -2,6 +2,6 @@ def configurations(embedding_dim=45, units=32, top_k=45, X_train, features_shape
     embedding_dim = embedding_dim
     units = units
     vocab_size = top_k + 1
-    num_steps = len(img_train) // 1
+    num_steps = len(X_train) // 1
     features_shape = features_shape
     attention_features_shape = attention_features_shape
